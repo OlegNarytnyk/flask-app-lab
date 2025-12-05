cf9c2f066512d6b3ddd38c3ad0499e052ff926ca
OlegNarytnyk <oleg.naritnik@gmail.com>
2025-12-05 00:18:00 -0600

    Lab2: Resume

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def main():
-    return 'Hello, world!'
 
-if __name__ == '__main__':
+@app.route("/")
+def resume():
+    return render_template("resume.html", title="Резюме")
+
+
+@app.route("/contacts", methods=["GET"])
+def contacts():
+    return render_template("contacts.html", title="Контакти")
+
+
+if __name__ == "__main__":
     app.run(debug=True)
